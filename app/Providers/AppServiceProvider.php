6875875180efe4dee91feb239b89500c10e71085
ceo-geekspeaker <249177309+ceo-geekspeaker@users.noreply.github.com>
2026-07-11 21:27:41 +0000

    chore: applied pint changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,4 +27,3 @@ class AppServiceProvider extends ServiceProvider
         }
     }
 }
-
